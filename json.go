5e5cfa11e6cdd5a349b5f3c24b267beef71f5649
Jarrod Seccombe <jarrod.seccombe@icloud.com>
2024-04-19 18:11:29 -0400

    add fixes from gosec

--- json.go
+++ json.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -27,5 +28,9 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		return
 	}
 	w.WriteHeader(code)
-	w.Write(dat)
+	_, err = w.Write(dat)
+
+	if err != nil {
+		fmt.Printf("Error writing JSON to response")
+	}
 }
